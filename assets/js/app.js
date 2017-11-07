69ab0e8c38ef7da796cff2ba52083e44934c7bdf
PazAutumn <pazcontrerastello@gmail.com>
2017-11-07 00:17:57 -0300

    textarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,11 @@ button.addEventListener("click", function add(){
         }, 1); 
       };
 })();
+
+var textarea = document.getElementById("textarea");
+var limit = 200;
+
+textarea.oninput = function() {
+  textarea.style.height = "";
+  textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
+};
