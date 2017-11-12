f66f9abb817859ddebb16253e884df9275f48eb5
PazAutumn <pazcontrerastello@gmail.com>
2017-11-11 23:12:04 -0300

    actualizando

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,74 +22,120 @@
       };
 })();*/
 
+var textarea = document.getElementById('textarea');
+var span = document.getElementById('span');
+var botoncito = document.getElementById('btn');
+
+textarea.onkeydown = function() {
+    //rescatamos el valor del textArea
+    var comment = document.getElementById('textarea').value;
+    //sacamos la longitud del mensaje
+    var long = comment.length;
+    //establecer un limite de caracteres
+    var limit = 140;
+    //contador con limite
+    var countdown = limit - (long + 1);
+    //vamos a rescatar
+    var counter = document.getElementById('counter-characters');
+    counter.innerHTML = countdown;
+
+    if (textarea.value.length < 120) {
+
+        span.setAttribute("color","yellow");
+
+    }
+
+    if (textarea.value.length < 130) {
+
+        span.setAttribute("color","yellow");
+
+    }
+
+    if (textarea.value.length < 0) {
+
+        span.setAttribute("color", "red");
+
+    }
+
+    if (textarea.value.length < 0 || textarea.value.length === 140) {
+
+        botoncito.setAttribute("disabled", "disabled");
+
+    } else {
+
+        botoncito.removeAttribute("disabled");
+
+    }
+
+}
+
 //agrandador de textarea
 
 var textarea = document.getElementById("textarea");
 var limit = 200;
 
 textarea.oninput = function() {
-  textarea.style.height = "";
-  textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
+    textarea.style.height = "";
+    textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
 };
 
 //cambiar de color el botoncito
 var botoncito = document.getElementById('btn');
 var textarea = document.getElementById("textarea");
 
-textarea.addEventListener("focus", function(){
-	if(textarea.length < 0 || textarea.length === 140){
-		botoncito.setAttribute("disabled","disabled");
-	}else{
-		botoncito.removeAttribute("disabled");
-	}
+textarea.addEventListener("focus", function() {
+    if (textarea.length < 0 || textarea.length === 140) {
+        botoncito.setAttribute("disabled", "disabled");
+    } else {
+        botoncito.removeAttribute("disabled");
+    }
 })
 
 //agregar comentario
 
 var botoncito = document.getElementById('btn');
 
-botoncito.addEventListener('click', function(){
-	var textarea = document.getElementById('textarea').value;
-	// el value sirve para tomar el valor del id comment
+botoncito.addEventListener('click', function() {
+    var textarea = document.getElementById('textarea').value;
+    // el value sirve para tomar el valor del id comment
 
-	//limpiar el textarea
-	document.getElementById('textarea').value='';
+    //limpiar el textarea
+    document.getElementById('textarea').value = '';
 
-	//contenedor que esta en html
-	var cont = document.getElementById('cont');
+    //contenedor que esta en html
+    var cont = document.getElementById('cont');
 
-	// creamos el div que contiene cada comentario
+    // creamos el div que contiene cada comentario
 
-	var newComments = document.createElement('div');
+    var newComments = document.createElement('div');
 
-	//validar que el textarea tenga un mensaje
+    //validar que el textarea tenga un mensaje
 
-	if (textarea.length == 0 || textarea == null){
-		alert('Deben ingresar un mensaje');
-		botoncito.setAttribute("disabled","disabled");
-		return false;
-	}
-
-		
-	//nodos de texto del textarea ya creada
-
-	var textNewComment = document.createTextNode(textarea);
-
-	var contenedorElemento = document.createElement('p');
-
-	contenedorElemento.appendChild(textNewComment);
-
-	newComments.appendChild(contenedorElemento);
-
-	cont.appendChild(newComments);
+    if (textarea.length == 0 || textarea == null) {
+        alert('Deben ingresar un mensaje');
+        botoncito.setAttribute("disabled", "disabled");
+        return false;
+    }
 
 
-/*validar que el mensaje tenga menos de 140 caracteres
+    //nodos de texto del textarea ya creada
 
-	if (charLeftLabel.length === 141){
-		alert("tu mensaje excede los 140 caractres");
-		botoncito.setAttribute("disabled","disabled");
-		return false;
-	}*/
+    var textNewComment = document.createTextNode(textarea);
+
+    var contenedorElemento = document.createElement('p');
+
+    contenedorElemento.appendChild(textNewComment);
+
+    newComments.appendChild(contenedorElemento);
+
+    cont.appendChild(newComments);
+
+
+    /*validar que el mensaje tenga menos de 140 caracteres
+
+    	if (charLeftLabel.length === 141){
+    		alert("tu mensaje excede los 140 caractres");
+    		botoncito.setAttribute("disabled","disabled");
+    		return false;
+    	}*/
 })
-
